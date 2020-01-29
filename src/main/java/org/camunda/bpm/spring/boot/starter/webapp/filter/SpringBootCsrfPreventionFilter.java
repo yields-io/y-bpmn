fb9b4c0f49042aa7b39a8cff41b87bb91a80559e
Adam Nowaczyk <adam.nowaczyk@acaisoft.com>
2020-01-29 09:58:31 +0100

    Disabled CSRF protection

---
+++ src/main/java/org/camunda/bpm/spring/boot/starter/webapp/filter/SpringBootCsrfPreventionFilter.java
@@ -0,0 +1,28 @@
+package org.camunda.bpm.spring.boot.starter.webapp.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SpringBootCsrfPreventionFilter implements Filter {
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+        filterConfig.getFilterName();
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        filterChain.doFilter(request, response);
+    }
+
+    public void destroy() {
+    }
+
+}
